330262eb63ddabdc00bb3173e465da53f90d98ff
zmaalick <zubair.maalick@metoffice.gov.uk>
2022-01-17 13:37:58 +0000

    add script to upload file on S3 #101

--- aws-scripts/s3_data_upload.py
+++ aws-scripts/s3_data_upload.py
@@ -1,2 +1,46 @@
 # this script load data on aws s3 bucket
 import boto3
+import os
+
+
+
+def upload_file(file_name, bucket, object_name=None):
+    """Upload a file to an S3 bucket
+
+    :param file_name: File to upload
+    :param bucket: Bucket to upload to
+    :param object_name: S3 object name. If not specified then file_name is used
+    :return: True if file was uploaded, else False
+    """
+
+    # If S3 object_name was not specified, use file_name
+    if object_name is None:
+        object_name = os.path.basename(file_name)
+
+    # Upload the file
+    s3_client = boto3.client('s3')
+    try:
+        response = s3_client.upload_file(file_name, bucket, object_name)
+    except ClientError as e:
+        logging.error(e)
+        return False
+    return True
+
+
+
+
+
+
+def main():
+    bucket = "ias-pyprecis"
+
+    # TODO: modify it to load multiple files 
+    file_name = "sample.txt"
+    object_name = "sample.txt"
+
+    upload_file(file_name, bucket, object_name=None)
+    
+
+
+if __name__ == "__main__":
+    main()
