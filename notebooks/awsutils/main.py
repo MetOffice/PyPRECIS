92b733b214533e2623df8f3f16929deb5888e375
zmaalick <zubair.maalick@metoffice.gov.uk>
2022-03-01 11:15:59 +0000

    add aws utils

---
+++ notebooks/awsutils/main.py
@@ -0,0 +1,42 @@
+from fetch_s3_file import copy_s3_files, find_matching_s3_keys
+import iris
+import os
+
+
+
+
+def load_data(inpath):
+
+    if inpath.startswith('s3'):
+        keys = find_matching_s3_keys(inpath)
+        s3dir = get_directory(inpath)
+        temp_path = '/scratch/zmaalick/'
+        for key in keys:
+            file = key.split('/')[-1]
+            if os.path.exists(os.path.join(temp_path,file)) == 0:
+                copy_s3_files(os.path.join(s3dir,key), temp_path)
+            else:
+                print(key, ' already exist')
+
+        files = inpath.split('/')[-1]
+        data = iris.load(os.path.join(temp_path,files))
+        return data
+
+
+def get_directory(inpath):
+    path = inpath.split('/')
+    dirpath='s3://'
+    for p in path[2:-1]:
+        dirpath = os.path.join(dirpath,p)
+    return dirpath
+
+
+
+def main():
+    inpath = 's3://ias-pyprecis/data/sample_data.nc'
+    data = load_data(inpath)
+    print(data)
+
+
+if __name__ == "__main__":
+    main()
