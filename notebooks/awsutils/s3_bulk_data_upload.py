9eabd4dba674ae5b8bddf4209f94b198a9085e0b
Nick Savage <nhsavage42@gmail.com>
2022-04-22 08:32:27 +0100

    Gitignore issue fixed

---
+++ notebooks/awsutils/s3_bulk_data_upload.py
@@ -0,0 +1,26 @@
+import os
+from tqdm import tqdm
+import boto3
+
+
+def upload_folder_to_s3(s3_client, s3bucket, input_dir, s3_path):
+    # This method uploads the directory (also subdirectories) to S3.
+    # You can also specify the s3 bucket directory where to store data
+    pbar = tqdm(os.walk(input_dir))
+    for path, subdirs, files in pbar:
+        for file in files:
+            dest_path = path.replace(input_dir, "").replace(os.sep, '/')
+            s3_file = f'{s3_path}/{dest_path}/{file}'.replace('//', '/')
+            local_file = os.path.join(path, file)
+            s3_client.upload_file(local_file, s3bucket, s3_file)
+            pbar.set_description(f'Uploaded {local_file} to {s3_file}')
+    print(f"Successfully uploaded {input_dir} to S3 {s3_path}")
+
+
+def main():
+    s3_client = boto3.client('s3')
+    upload_folder_to_s3(s3_client, 'ias-pyprecis', '/data/users/fris/s3_uploads/pp', 'data/pp')
+
+
+if __name__ == "__main__":
+    main()
