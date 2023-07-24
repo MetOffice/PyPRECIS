be95fd15aad8273da73d1c6cf248b2584d20ccc6
Nick Savage <nhsavage@users.noreply.github.com>
2023-07-24 13:20:28 +0100

    Fix/copyright (#159)
    * et of copyright notices
    
    * add reamining copyright notices
    
    * start adding copyright headers to notebooks

--- notebooks/awsutils/s3_bulk_data_upload.py
+++ notebooks/awsutils/s3_bulk_data_upload.py
@@ -1,3 +1,8 @@
+'''
+(C) Crown Copyright, Met Office. All rights reserved.
+This file is part of PyPrecis and is released under the BSD 3-Clause license.
+See LICENSE in the root of the repository for full licensing details.
+'''
 import os
 from tqdm import tqdm
 import boto3
